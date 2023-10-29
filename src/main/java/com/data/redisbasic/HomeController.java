7b4d5726e66206f4e2524e9d135413aac7931526
devfrom2ne1 <devfrom2ne1@gmail.com>
2023-10-29 19:36:53 +0900

    Initial commit

---
+++ src/main/java/com/data/redisbasic/HomeController.java
@@ -0,0 +1,36 @@
+package com.data.redisbasic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/home")
+    public String home() {
+        return "Home Controller";
+    }
+
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name){
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("fruit", name);
+
+        return "Saved";
+    }
+
+    @GetMapping("/getFruit")
+    public String getFruit(){
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String fruitName = ops.get("fruit");
+
+        return fruitName;
+    }
+}
